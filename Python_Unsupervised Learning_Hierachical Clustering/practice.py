44f9992c1a14e65851966b37b1dfe58d1daf391f
Jack(TaeYoun) Kwon <kweont0211@gmail.com>
2022-10-28 14:29:58 +0900

    add Silhouette Test

--- Python_Unsupervised Learning_Hierachical Clustering/practice.py
+++ Python_Unsupervised Learning_Hierachical Clustering/practice.py
@@ -71,4 +71,56 @@ plt.subplot(224)
 plt.scatter(data[:,0],data[:,1],c=label)
 
 plt.savefig('clustering Figures.png')
-plt.show()
+# plt.show()
+
+
+from scipy.cluster.hierarchy import dendrogram
+plt.figure(figsize=(10,10))
+children = single_clustering.children_
+distance = np.arange(children.shape[0])
+no_of_observations = np.arange(2, children.shape[0]+2)
+linkage_matrix = np.column_stack([children, distance, no_of_observations]).astype(float)
+
+dendrogram(linkage_matrix, p=len(data), labels=single_cluster,show_contracted=True, no_labels=True)
+
+# Silhouette Score
+
+from sklearn.metrics import silhouette_score
+from sklearn.cluster import KMeans
+
+kmeans = KMeans(n_clusters=3)
+kmeans.fit(data)
+cluster = kmeans.predict(data)
+
+best_n = 1
+best_score = -1
+
+for n_cluster in range(2,11):
+  kmeans = KMeans(n_clusters=n_cluster)
+  kmeans.fit(data)
+  cluster = kmeans.predict(data)
+  score = silhouette_score(data, cluster)
+
+  print('Number of Cluster: {} Silhoutette Score:{:.2f}'.format(n_cluster, score))
+
+  if score > best_score:
+    best_n = n_cluster
+    best_score = score
+
+print('Number of Cluster that has Max Sillhoutette Score: {}\n Silhoutette Score {:.2f}'.format(best_n, best_score))
+print('\n')
+print('New Silhouette Test')
+
+for n_cluster in range(2,11):
+  average_clustering = AgglomerativeClustering(n_clusters=n_cluster,linkage='average')
+  average_clustering.fit(data)
+  cluster = average_clustering.labels_
+  score = silhouette_score(data, cluster)
+
+  print('Number of Cluster: {} Silhoutette Score:{:.2f}'.format(n_cluster, score))
+
+  if score > best_score:
+    best_n = n_cluster
+    best_score = score
+
+print('Number of Cluster that has Max Sillhoutette Score: {}\n Silhoutette Score {:.2f}'.format(best_n, best_score))
