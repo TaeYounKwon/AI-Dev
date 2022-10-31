8e40f71f2c92abfaf0255dcd47fba3a919f8679e
Jack(TaeYoun) Kwon <kweont0211@gmail.com>
2022-10-31 10:40:24 +0900

    Exponential

---
+++ Python_Baisc Perceptron_Math/Exponential.py
@@ -0,0 +1,20 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
+
+# y= ax
+def exponential_function(x):
+    a= 4
+    
+    return a**x 
+
+x = np.arange(-3,2,0.1)
+y = exponential_function(x)
+
+plt.plot(x,y)
+plt.xlabel('x-Value')
+plt.ylabel('y-Value')
+plt.title('Exponential Function Graph')
+plt.savefig('Exponential_Graph.png')
+plt.show()
